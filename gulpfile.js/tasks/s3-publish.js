d955c595ede6bdc4dc26485cf469098d4d1b5b55
rudywaltz <david.barkoczi@gmail.com>
2016-03-19 21:11:54 +0100

    add aws-publish

---
+++ gulpfile.js/tasks/s3-publish.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var gulp = require('gulp');
+var config = require('../config/index');
+var awspublish = require('gulp-awspublish');
+// var argv = require('yargs').argv;
+
+
+var publisher =  awspublish.create({
+  params: {
+    Bucket: '	gombapresszo-podcast'
+  }
+});
+
+
+var headers = {
+  'Cache-Control': 'max-age=315360000, no-transform, public',
+  'x-amz-acl': 'bucket-owner-full-control'
+};
+
+gulp.task('s3-publish', function() {
+  return gulp.src('./dest/**/*')
+       // gzip, Set Content-Encoding headers and add .gz extension
+      // .pipe(awspublish.gzip({ ext: '.gz' }))
+
+      // publisher will add Content-Length, Content-Type and headers specified above
+      // If not specified it will set x-amz-acl to public-read by default
+      .pipe(publisher.publish(headers))
+
+      // create a cache file to speed up consecutive uploads
+      .pipe(publisher.cache())
+
+       // print upload updates to console
+      .pipe(awspublish.reporter());
+});
