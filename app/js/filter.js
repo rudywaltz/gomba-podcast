6ffc1f58a4629de1a9a3de5c4d7f718dca7569d4
rudywaltz <david.barkoczi@gmail.com>
2016-03-24 16:32:36 +0100

    feat(filter): add old show filter

--- app/js/filter.js
+++ app/js/filter.js
@@ -6,8 +6,27 @@ var options = {
   valueNames: [ 'podcast__title', 'content' ]
 };
 
+var filterOldShow = function (list) {
+  var element = document.getElementById('oldShow');
+  element.addEventListener('change', function() {
+    console.log('change');
+    list.filter(function(item) {
+      var oldShow = 'régi';
+      if (item.values().content.toLowerCase().indexOf(oldShow) > -1 ||
+        item.values().podcast__title.toLowerCase().indexOf(oldShow) > -1) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    return false;
+  }, false);
+};
+
+
 var initList = function () {
   var podcastList = new List('podcast-wrapper', options);
+  filterOldShow(podcastList);
 };
 
 module.exports = {
