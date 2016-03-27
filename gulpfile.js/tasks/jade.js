e007c70f25a3c6c336cab9a64ceac183ad960133
rudywaltz <david.barkoczi@gmail.com>
2016-03-27 23:18:53 +0200

    feat(layout): add rev to css

--- gulpfile.js/tasks/jade.js
+++ gulpfile.js/tasks/jade.js
@@ -3,12 +3,15 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var browserSync = require('browser-sync');
+var revReplace = require('gulp-rev-replace');
 var config = require('../config/jade');
 
 gulp.task('jade', function() {
+  var manifest = gulp.src(config.dest + '/rev-manifest.json');
 
   gulp.src(config.src)
     .pipe(jade(config.src))
+    .pipe(revReplace({manifest: manifest}))
     .pipe(gulp.dest(config.dest))
     .pipe(browserSync.reload({ stream: true }))
 });
